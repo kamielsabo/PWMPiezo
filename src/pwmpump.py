dcc8b005d81f2443089400d76f0a20d748260efe
kamielsabo <mandela.dehondt@gmail.com>
2022-03-24 17:14:44 +0100

    editet digitalouttest.py

--- src/pwmpump.py
+++ src/pwmpump.py
@@ -6,11 +6,10 @@ from datetime import datetime, timedelta
 out = gpiozero.PWMOutputDevice(13)
 out.frequency = 100
 out.value = 0.3
-newNow = datetime.now() + timedelta(seconds=100)
+newNow = datetime.now() + timedelta(seconds=10)
 booleanTime = True
 
 while booleanTime:
-    #out.on()
     if newNow - datetime.now() < timedelta(seconds=0):
         booleanTime = False
 

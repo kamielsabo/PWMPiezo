02732fee61e96415a120a6ea96bae34b4f327b62
kamielsabo <mandela.dehondt@gmail.com>
2022-03-29 12:52:04 +0200

    editet digitalouttest.py

--- src/pwmmotor.py
+++ src/pwmmotor.py
@@ -2,11 +2,4 @@ from gpiozero import Servo
 from time import sleep
 
 servo = Servo(12)
-
-while True:
-    servo.min()
-    sleep(5)
-    servo.mid()
-    sleep(5)
-    servo.max()
-    sleep(5)
+servo.max()

d6b4079600b12be76eafe43d60b81c9fb0e017b6
kamielsabo <mandela.dehondt@gmail.com>
2022-03-29 17:47:20 +0200

    added pwmpumptest.py

---
+++ pwmpumptest.py
@@ -0,0 +1,16 @@
+import gpiozero
+import time
+from datetime import datetime, timedelta
+
+out = gpiozero.PWMOutputDevice(13)
+out.frequency = 200
+out.value = 0.2
+newNow = datetime.now() + timedelta(seconds=secondsFlow)
+booleanTime = True
+
+while booleanTime:
+    if newNow - datetime.now() < timedelta(seconds=0):
+        booleanTime = False
+
+out.off()
+

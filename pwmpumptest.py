a254cd8efc9dc7ecc430660aec45dd0452df6800
kamielsabo <mandela.dehondt@gmail.com>
2022-03-29 17:55:37 +0200

    added pwmpumptest.py

--- pwmpumptest.py
+++ pwmpumptest.py
@@ -2,10 +2,12 @@ import gpiozero
 import time
 from datetime import datetime, timedelta
 
+volume = 15     #volume in cl
+timeToRun = (10*volume) / 29.52
 out = gpiozero.PWMOutputDevice(13)
 out.frequency = 200
 out.value = 0.2
-newNow = datetime.now() + timedelta(seconds=5)
+newNow = datetime.now() + timedelta(seconds=timeToRun)
 booleanTime = True
 
 while booleanTime:

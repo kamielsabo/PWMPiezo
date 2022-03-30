784657b10ac0eeb712b106191e51413fdba9b8a8
kamielsabo <mandela.dehondt@gmail.com>
2022-03-30 23:46:26 +0200

    added pwmpumptest.py

--- src/temperature_sensor.py
+++ src/temperature_sensor.py
@@ -2,6 +2,6 @@ import sensors
 
 data = sensors.ReadChannel(0)
 volts = sensors.ConvertVolts(data, 3)
-print("Volts: " + volts)
+print("Volts: " + str(volts))
 temp = sensors.ConvertTemp(volts, 2)
 print(temp)

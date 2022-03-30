5e2c060de37ac8cd1a8dd49dc989ecb460ca04e2
kamielsabo <mandela.dehondt@gmail.com>
2022-03-31 01:46:24 +0200

    added pwmpumptest.py

--- src/databank_retriever.py
+++ src/databank_retriever.py
@@ -5,11 +5,19 @@ class DatabankRetriever:
     def __init__(self):
         self.upcomming_settings = requests.get('https://studev.groept.be/api/a21ib2b02/get_coffee_next').json()
 
+    def update(self):
+        self.upcomming_settings = requests.get('https://studev.groept.be/api/a21ib2b02/get_coffee_next').json()
+
     def get_temperature(self):
+        self.update()
         return str(self.upcomming_settings[0]["temp"])
 
     def get_volume(self):
+        self.update()
         return int(self.upcomming_settings[0]['volume'])
 
     def get_alarm_time(self):
+        self.update()
         return self.upcomming_settings[0]["alarm_datetime"]
+
+
